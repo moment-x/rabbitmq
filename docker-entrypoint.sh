735be6e3663d141cef38d3832f3f8a41b7b61097
fast2beer <token@email.com>
2015-07-27 17:45:18 +0800

    g

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
-sudo rabbitmqctl add_user user password
-sudo rabbitmqctl add_vhost vhost
-sudo rabbitmqctl set_user_tags user tag
-sudo rabbitmqctl set_permissions -p vhost user ".*" ".*" ".*"
+rabbitmqctl add_user user password
+rabbitmqctl add_vhost vhost
+rabbitmqctl set_user_tags user tag
+rabbitmqctl set_permissions -p vhost user ".*" ".*" ".*"
 
 rabbitmq-server
